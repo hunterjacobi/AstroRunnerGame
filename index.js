b00ed1f025f29f088a860196b6d78d68654d0a2d
Hunter <hunterjacobibrowning@gmail.com>
2022-05-26 10:23:45 -0400

    Added background scrolling and win scenario

--- index.js
+++ index.js
@@ -38,10 +38,10 @@ class Player {
 }
 
 class Platform {
-    constructor() {
+    constructor({x, y}) {
         this.position = {
-            x: 200,
-            y: 100
+            x,
+            y
         }
 
         this.width = 200
@@ -55,7 +55,9 @@ class Platform {
 }
 
 const player = new Player();
-const platform = new Platform();
+const platforms = [new Platform({
+    x: 200, y: 100}), new Platform({
+        x:500,y:200})]
 
 const keys = {
     right: {
@@ -66,26 +68,50 @@ const keys = {
     }
 }
 
+let scrollOffset = 0
+
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0,0,canvas.width,canvas.height);
     player.update();
-    platform.draw();
+    platforms.forEach((platform) => {
+        platform.draw();
+    });
 
-    if (keys.right.pressed) {
+    if (keys.right.pressed && player.position.x < 400) {
         player.velocity.x = 5;
-    } else if (keys.left.pressed) {
+    } else if (keys.left.pressed && player.position.x > 100) {
         player.velocity.x = -5;
     }
-    else player.velocity.x = 0;
+    else {
+        player.velocity.x = 0;
+
+        if (keys.right.pressed) {
+            scrollOffset += 5;
+            platforms.forEach((platform) => {
+                platform.position.x -= 5
+            });
+        } else if (keys.left.pressed) {
+            scrollOffset -= 5;
+            platforms.forEach((platform) => {
+                platform.position.x += 5
+            });
+        }
+    } 
 
     // Platform collision detection
+    platforms.forEach((platform) => {
     if (player.position.y + player.height <= platform.position.y &&
          player.position.y + player.height + player.velocity.y >= platform.position.y
          && player.position.x + player.width >= platform.position.x
          && player.position.x <= platform.position.x + platform.width)
     {
         player.velocity.y = 0;
+    }
+})
+
+    if (scrollOffset > 2000) {
+        console.log('you win!')
     }
 }
 
@@ -128,7 +154,7 @@ addEventListener('keyup', ({ keyCode }) => {
             break
         case 87:
             console.log('up')
-            player.velocity.y -= 20;
+            player.velocity.y -= 5;
             break
     }
     console.log(keys.right.pressed)
