a7879758557a9310b1070ed5c043dd053f48fcd4
Hunter <hunterjacobibrowning@gmail.com>
2022-05-26 12:06:08 -0400

    Added Image platforms

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,8 +1,10 @@
+import platformImg from'../img/platform.png'
+console.log(platformImg)
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+canvas.width = 1024;
+canvas.height = 576;
 
 const gravity = 0.5;
 
@@ -38,26 +40,44 @@ class Player {
 }
 
 class Platform {
-    constructor({x, y}) {
+    constructor({x, y, image}) {
         this.position = {
             x,
             y
         }
 
-        this.width = 200
-        this.height = 20
+        this.image = image
+
+        this.width = image.width
+        this.height = image.height
+
+        console.log(this.width + 'x' +
+    this.height)
     }
 
     draw() {
-        c.fillStyle = 'blue'
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        c.drawImage(this.image, this.position.x, this.position.y)
     }
 }
 
+const image = new Image()
+image.onload = function() {
+  this.width
+  this.height
+  console.log(this.width + 'x' +
+    this.height)
+}
+image.src = platformImg
+
+console.log(image)
+
 const player = new Player();
 const platforms = [new Platform({
-    x: 200, y: 100}), new Platform({
-        x:500,y:200})]
+    x: -1,
+    y: 470,
+    image
+    }), 
+    new Platform({x:image.width -3, y:470, image})]
 
 const keys = {
     right: {
@@ -72,11 +92,12 @@ let scrollOffset = 0
 
 function animate() {
     requestAnimationFrame(animate);
-    c.clearRect(0,0,canvas.width,canvas.height);
-    player.update();
+    c.fillStyle = 'white'
+    c.fillRect(0,0,canvas.width,canvas.height);
     platforms.forEach((platform) => {
         platform.draw();
     });
+    player.update();
 
     if (keys.right.pressed && player.position.x < 400) {
         player.velocity.x = 5;
